2b42388a74816b1e5d5ebc71f89cb71a38892f9b
Enviviety <149268451+Enviviety@users.noreply.github.com>
2023-11-01 00:59:58 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,4 +16,86 @@ and...
                                                                     
 ]]
 
-local v0=Instance.new("ScreenGui");local v1=Instance.new("Frame");local v2=Instance.new("TextLabel");local v3=Instance.new("TextBox");local v4=Instance.new("TextButton");v0.Name="PopupFrame";v0.Parent=game:GetService("CoreGui");v0.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;v1.Name="Popup";v1.Parent=v0;v1.BackgroundColor3=Color3.fromRGB(255,255,255);v1.BackgroundTransparency=0.5;v1.BorderColor3=Color3.fromRGB(0,0,0);v1.Position=UDim2.new(0.0436817482,0,0.697406352,0);v1.Size=UDim2.new(0,177,0,143);v1.Active=true;v1.Draggable=true;v2.Parent=v1;v2.BackgroundColor3=Color3.fromRGB(255,255,255);v2.BackgroundTransparency=0.5;v2.BorderColor3=Color3.fromRGB(0,0,0);v2.Size=UDim2.new(0,177,0,34);v2.Font=Enum.Font.Code;v2.Text="Tools Duper";v2.TextColor3=Color3.fromRGB(0,0,0);v2.TextScaled=true;v2.TextSize=14;v2.TextWrapped=true;v3.Parent=v1;v3.BackgroundColor3=Color3.fromRGB(255,255,255);v3.BackgroundTransparency=0.6;v3.BorderColor3=Color3.fromRGB(0,0,0);v3.BorderSizePixel=0;v3.Position=UDim2.new(0.0564971752,0,0.321678311,0);v3.Size=UDim2.new(0,155,0,50);v3.Font=Enum.Font.Code;v3.Text="50";v3.TextColor3=Color3.fromRGB(0,0,0);v3.TextScaled=true;v3.TextSize=14;v3.TextWrapped=true;v4.Parent=v1;v4.BackgroundColor3=Color3.fromRGB(255,255,255);v4.BackgroundTransparency=0.5;v4.BorderColor3=Color3.fromRGB(0,0,0);v4.BorderSizePixel=0;v4.Position=UDim2.new(0.107344635,0,0.74125874,0);v4.Size=UDim2.new(0,137,0,30);v4.Font=Enum.Font.Code;v4.Text="Dupe";v4.TextColor3=Color3.fromRGB(0,0,0);v4.TextScaled=true;v4.TextSize=14;v4.TextWrapped=true;v4.MouseButton1Click:Connect(function()local v56=Instance.new("Hint",workspace);local v57=tonumber(v2.Text);if ((v57==nil) or (v57>30)) then task.spawn(function()v56.Text="Error: the text value is nil or higher than 30";wait(5);v56.Text="";end);end v56.Text="Loading...";wait(math.random(0.01,0.5));v56.Text="Starting dupe...";wait(1);for v59=1,v57 do task.spawn(function()for v61=1,v57 do game:GetService("ReplicatedStorage"):WaitForChild("Events"):FindFirstChildWhichIsA("RemoteFunction"):InvokeServer("b̴̨̨͕̹̼̳͔͎͍͗͆͐ͬ̅ͨͦ̐̑͘͠ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠b̴̨̨͕̹̼̳͔͎͍͗͆͐ͬ̅ͨͦ̐̑͘͠f̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞a̷̶̢̛͕̝͙͇͇̦̝̺̙̭̝̗̝̗͍̤̝̤̝̦̹̝̲̯̝̠͕̐͑᷀̐ͧ͒̀̐͑̇᷄ͤ͊̓ͭ͑͑᷁ͧ̅̃͊͊͑̓ͪ̚͟͟͝͠ͅḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥį̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅf̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘f̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘ḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞į̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅh͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡į̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠h͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠h͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠"   .. tostring(math.random(1,9999999)) ,"Spinner");end end);end wait(5);v56:Destroy();end);
+local PopupFrame = Instance.new("ScreenGui")
+local Popup = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local TextButton = Instance.new("TextButton")
+
+PopupFrame.Name = "PopupFrame"
+PopupFrame.Parent = game:GetService("CoreGui")
+PopupFrame.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Popup.Name = "Popup"
+Popup.Parent = PopupFrame
+Popup.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Popup.BackgroundTransparency = 0.500
+Popup.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Popup.Position = UDim2.new(0.0436817482, 0, 0.697406352, 0)
+Popup.Size = UDim2.new(0, 177, 0, 143)
+Popup.Active = true
+Popup.Draggable = true
+
+TextLabel.Parent = Popup
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 0.500
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.Size = UDim2.new(0, 177, 0, 34)
+TextLabel.Font = Enum.Font.Code
+TextLabel.Text = "Tools Duper"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+TextBox.Parent = Popup
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BackgroundTransparency = 0.600
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.0564971752, 0, 0.321678311, 0)
+TextBox.Size = UDim2.new(0, 155, 0, 50)
+TextBox.Font = Enum.Font.Code
+TextBox.Text = "50"
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+TextButton.Parent = Popup
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 0.500
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.107344635, 0, 0.74125874, 0)
+TextButton.Size = UDim2.new(0, 137, 0, 30)
+TextButton.Font = Enum.Font.Code
+TextButton.Text = "Dupe"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+TextButton.MouseButton1Click:Connect(function()
+	local h = Instance.new("Hint", workspace)
+	local power = tonumber(TextLabel.Text)
+	if power == nil or power > 30 then
+		task.spawn(function()
+			h.Text = ("Error: the text value is nil or higher than 30")
+			wait(5)
+			h.Text = ""
+		end)
+	end
+	h.Text = "Loading..."
+	wait(math.random(0.01, 0.5))
+	h.Text = "Starting dupe..."
+	wait(1)
+	for i = 1, power do
+		task.spawn(function()
+			for i = 1, power do
+				game:GetService("ReplicatedStorage"):WaitForChild("Events"):FindFirstChildWhichIsA("RemoteFunction"):InvokeServer("b̴̨̨͕̹̼̳͔͎͍͗͆͐ͬ̅ͨͦ̐̑͘͠ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠b̴̨̨͕̹̼̳͔͎͍͗͆͐ͬ̅ͨͦ̐̑͘͠f̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘ģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞a̷̶̢̛͕̝͙͇͇̦̝̺̙̭̝̗̝̗͍̤̝̤̝̦̹̝̲̯̝̠͕̐͑᷀̐ͧ͒̀̐͑̇᷄ͤ͊̓ͭ͑͑᷁ͧ̅̃͊͊͑̓ͪ̚͟͟͝͠ͅḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥį̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅf̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘f̴͓̜̲̺̥̙̯᷊͕̫͉̠᷅̈́̈́᷆ͯͬ͌̈́̋́ͪ̈́͒͂̚̚͘̚̚̚̚̚̚̚̚͘ḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥģ̧̧̧̧̧̖̮ͭͭͭͭ̂ͭͭͭͭ᷾ͭͭͫͭͭ͏̸̸̧̧̧̧̢̧̧̧̧̧̧̧̧̧̧̣͓͎ͭͭ̉ͭͭ͗ͭͭͭͭͭͭ᷆ͭͭ̎ͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭͭ͘̕͞į̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅh͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡į̸̵̷̡̡̛͔̻͔͈͔͔̲͔͔͔̣͔͔͔͔̳͔͔͉͔͔͔͔͔͔͔̲͔͔͔̟͔͔͉͔͔͔͔͔͔̦͔͔͔͔͔͔̩͔͔͔̪͔͔͔͔̖͔͔͔͔͔͔͙͔͔͔͔͔͔͔͔͔͔͔͔͕͔͕͔͔͔͔͔͔̝͔̘͔̳͔̏̑̒̔̇͌͑̏͗̊́̇̂̇ͮ᷀͐ͮͣ͐ͭ̇̎᷾̋ͥ͛᷅᷃͛̓̇̐̕͘͟͢͟͝͡͡͝ͅͅͅḑ͇̗̬̜͐᷀᷄᷀̄̓᷀́̾᷀͏͙̜͗᷀͏̈᷀ͥȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠h͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠h͛͊͏̵̠͎̯͉̰͙ͮͥ̓᷇͘͘͜͟͏̣̮͎͚͙͇́͐͆͑͘͞͏̶̷̶̡̨̗͚̺͚̙᷂̼͎̗̟̣͉̞̦̻̙͈᷾᷁̆͛̅ͬ͗᷅̃̌͗̊̌᷄̈̇ͬ̏ͮ̍̿̄͘͘͘̚͘͘͢͟͡ȩ̴̴͈̘͌ͯͪ̂͗͑͒ͤͦͩ͠".. tostring(math.random(1, 9999999)), "Spinner")
+			end
+		end)
+	end
+	wait(5)
+	h:Destroy()
+end)
